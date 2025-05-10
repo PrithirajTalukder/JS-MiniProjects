62503647801ab15ec65c2de6db580d441ea6c968
Prithiraj Talukder <PrithirajTalukder@gmail.com>
2025-05-11 02:12:17 +0600

    rectanle color change from one side to another

--- RectangleMove/script.js
+++ RectangleMove/script.js
@@ -1,6 +1,47 @@
- var rect = document.querySelector("#center").addEventListener("mousemove", function(){
-     this.style.backgroundColor= "black";
+ var rect = document.querySelector("#center");
+ rect.addEventListener("mousemove", function(details){
+
+    var rectanglelocation = rect.getBoundingClientRect();
+    var insideval = details.clientX - rectanglelocation.left;
+
+    if (insideval < rectanglelocation.width/2){
+        
+        var redcolor = gsap.utils.mapRange(
+            0, rectanglelocation.width/2, 255, 0, insideval
+        );
+
+        gsap.to(rect, {
+            backgroundColor: `rgb(${redcolor}, 0, 0)`,
+            ease: Power4,
+        } )
+
+
+
+
+
+
+
+
+    }else{
+         var bluecolor = gsap.utils.mapRange(
+            rectanglelocation.width/2, rectanglelocation.width, 0, 255, insideval
+        );
+
+        gsap.to(rect, {
+            backgroundColor: `rgb(0, 0, ${bluecolor})`,
+            ease: Power4,
+        } )
+
+
+    }
+
     
+ })
+
+ rect.addEventListener("mouseleave", function(){
+    gsap.to(rect,{
+        backgroundColor: "white",
+    })
  })
    
  

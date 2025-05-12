72c0cf0fa45ddb33c45dc067053796afd437bea3
Prithiraj Talukder <PrithirajTalukder@gmail.com>
2025-05-12 23:59:42 +0600

    modified:   MouseFollower/script.js

--- MouseFollower/script.js
+++ MouseFollower/script.js
@@ -5,7 +5,7 @@ window.addEventListener("mousemove", function(dets){
     var xval = gsap.utils.mapRange(0, window.innerWidth, 200 + rect.getBoundingClientRect().width/2, window.innerWidth - (200 + rect.getBoundingClientRect().width/2), dets.clientX)
    gsap.to("#rect", {
     left: xval + "px",
-    ease: Power3,
+    ease: Power3,jjjj
 
    })
 })

45f75b3269539e71859c6d1db6b0df2166c0a9a8
Prithiraj Talukder <PrithirajTalukder@gmail.com>
2025-05-18 03:19:22 +0600

    modified:   instastory/index.html 	modified:   instastory/script.js 	modified:   instastory/style.css

---
+++ instastory/script.js
@@ -0,0 +1,35 @@
+var arr = [
+    {
+        dp: "https://images.unsplash.com/photo-1529139574466-a303027c1d8b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story:"https://images.unsplash.com/photo-1529047911987-2f07ecafad0c?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }, {
+        dp: "https://images.unsplash.com/photo-1529139574466-a303027c1d8b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story:"https://images.unsplash.com/photo-1529047911987-2f07ecafad0c?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }, {
+        dp: "https://images.unsplash.com/photo-1529139574466-a303027c1d8b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story:"https://images.unsplash.com/photo-1529047911987-2f07ecafad0c?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }, {
+        dp: "https://images.unsplash.com/photo-1529139574466-a303027c1d8b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story:"https://images.unsplash.com/photo-1529047911987-2f07ecafad0c?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+]
+
+
+var clutter = "";
+var story = document.querySelector("#story");
+
+arr.forEach(function(elem, idx){
+    clutter += ` <div id="circle">
+            <img id=${idx} src="${elem.dp}" alt="">
+        </div>`
+})
+
+story.innerHTML= clutter;
+
+story.addEventListener("click", function(dets){
+    document.querySelector("#big").style.display = "block"
+    document.querySelector("#big").style.backgroundImage = `url(${arr[dets.target.id].story})`
+
+
+    setTimeout(function(){
+document.querySelector("#big").style.display = "none"
+        
+
+    }, 3000)
+});
